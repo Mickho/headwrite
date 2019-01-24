cbb0457ce27fa17634f10aecdbd9e6749f9e8649
Mick Ho <mick.my.ho@gmail.com>
2019-01-24 22:00:37 +0800

    Add file

---
+++ headwrite.py
@@ -0,0 +1,14 @@
+products = []
+while True:
+		a = input('Please input number:')
+		if a == 'q':
+			break
+		price = input('Please input price:')
+		p = []
+		p.append(a)
+		p.append(price)
+		products.append(p)
+with open('Test.csv', 'w', encoding = 'UTF-8') as f:
+	f.write('Number,Price\n')
+	for i in products:
+		f.write(str(i) + '\n')        
